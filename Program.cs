0c60002968f9a1e69cf310f3e6a0a5c21253fc17
H4ymak3r <mirko.damonte@gmail.com>
2024-10-16 12:23:04 +0200

    Aggiungere i file di progetto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿List<string> contatti = new List<string> {};
+Console.Write("Scrivi il contatto per aggiungerlo o rimuoverlo: ");
+string newcontact = Console.ReadLine();
+int control = 0;
+if (!contatti.Contains(newcontact))
+{
+    contatti.Add(newcontact);
+}
+
+Console.WriteLine("I contatti sono: ");
+
+foreach  (string contact in contatti)
+{
+    Console.Write($"{contact} ");
+}

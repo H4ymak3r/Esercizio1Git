1aa3e617fce3c804cf2d92cac79779a9a430977c
H4ymak3r <mirko.damonte@gmail.com>
2024-10-16 13:05:46 +0200

    Aggiunta punto 3

--- Program.cs
+++ Program.cs
@@ -3,17 +3,24 @@ List<string> contacts = new List<string>();
 
 Console.Write("Write the contact to add: ");
 newcontact = Console.ReadLine();
-    if (!contacts.Contains(newcontact))
-    {
-        contacts.Add(newcontact);
-    }
-    else
-    {
-        Console.WriteLine("You already have this contact!");
-    }
+if (!contacts.Contains(newcontact))
+{
+    contacts.Add(newcontact);
+}
+else
+{
+    Console.WriteLine("You already have this contact!");
+}
 
 Console.Write("Your contacts: ");
 foreach (string contact in contacts)
 {
-    Console.Write($"{contact} ");
+    Console.WriteLine($"{contact} ");
+}
+
+Console.Write("Write the contact to check: ");
+newcontact = Console.ReadLine();
+if (contacts.Contains(newcontact))
+{
+    Console.WriteLine($"{newcontact} is saved in your contacts.");
 }
